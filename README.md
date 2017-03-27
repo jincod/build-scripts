3f0b7d32d5f40baffc44013d99660cccb590aea4
jincod <vadim.abdrashitov@gmail.com>
2017-03-27 18:00:52 +0500

    added cake script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Build scripts
+
+Simple script for build C# projects using [Cake](https://github.com/cake-build/cake).
+
+## Installation
+
+```powershell
+. { iwr -useb https://raw.githubusercontent.com/jincod/build-scripts/master/install.ps1 } | iex;
+```
---
+++ install.ps1
@@ -0,0 +1,4 @@
+$baseUrl = "https://raw.githubusercontent.com/jincod/build-scripts/master/"
+
+Invoke-WebRequest http://cakebuild.net/download/bootstrapper/windows -OutFile build.ps1
+Invoke-WebRequest "${baseUrl}/build-scripts/build.cake" -OutFile build.cake
