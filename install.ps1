4378f382042635a079fa97206ef0cc014fd970b9
jincod <vadim.abdrashitov@gmail.com>
2017-03-28 12:04:57 +0500

    added confirm

--- install.ps1
+++ install.ps1
@@ -1,5 +1,19 @@
 $baseUrl = "https://raw.githubusercontent.com/jincod/build-scripts/master/"
 
-Invoke-WebRequest http://cakebuild.net/download/bootstrapper/windows -OutFile build.ps1
-Invoke-WebRequest "${baseUrl}/build-scripts/build.cake" -OutFile build.cake
-Invoke-WebRequest "${baseUrl}/build-scripts/build.cmd" -OutFile build.cmd
+function downloadWithConfirm ($url, $out) {
+    if (Test-Path $out) {
+        Write-Host "$out exists. Override: Y/N?"
+
+        if ((read-host) -like "*y*") {
+            Invoke-WebRequest $url -OutFile $out
+        } else {
+            Write-Host "Skip $out"
+        }
+    } else {
+        Invoke-WebRequest $url -OutFile $out
+    }
+}
+
+downloadWithConfirm http://cakebuild.net/download/bootstrapper/windows build.ps1
+downloadWithConfirm "${baseUrl}/build-scripts/build.cake" build.cake
+downloadWithConfirm "${baseUrl}/build-scripts/build.cmd" build.cmd
